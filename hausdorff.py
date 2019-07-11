e3b63c6c0533cef0a03d60d7b2d8fb09c9556fc2
Martín Villanueva <martin.villanueva@usm.cl>
2019-07-11 18:33:17 -0400

    Removes old hausdorff c extension

--- hausdorff.py
+++ hausdorff.py
@@ -1,16 +1,23 @@
-import numba
 import numpy as np
-from distances import *
+import numba
+import distances
+from inspect import getmembers
+
+def _find_available_functions(module_name):
+    all_members = getmembers(module_name)
+    available_functions = [member[0] for member in all_members 
+                           if isinstance(member[1], numba.targets.registry.CPUDispatcher)]
+    return available_functions
 
 @numba.jit(nopython=True, fastmath=True)
-def _hausdorff(XA, XB, distance_func):
+def _hausdorff(XA, XB, distance_function):
 	nA = XA.shape[0]
 	nB = XB.shape[0]
 	cmax = 0.
 	for i in range(nA):
 		cmin = np.inf
 		for j in range(nB):
-			d = distance_func(XA[i,:], XB[j,:])
+			d = distance_function(XA[i,:], XB[j,:])
 			if d<cmin:
 				cmin = d
 			if cmin<cmax:
@@ -20,7 +27,7 @@ def _hausdorff(XA, XB, distance_func):
 	for j in range(nB):
 		cmin = np.inf
 		for i in range(nA):
-			d = distance_func(XA[i,:], XB[j,:])
+			d = distance_function(XA[i,:], XB[j,:])
 			if d<cmin:
 				cmin = d
 			if cmin<cmax:
@@ -30,13 +37,13 @@ def _hausdorff(XA, XB, distance_func):
 	return cmax
 
 def hausdorff(XA not None, XB not None, distance="euclidean"):
-	assert distance in distances_mapping, "distance must be one of the following: " + " ".join(distances_mapping.keys())
+    assert distance in _find_available_functions(distances), 'Distance is not an implemented function'
     assert type(XA) is np.ndarray and type(XB) is np.ndarray, "Arrays must be of type numpy.ndarray"
     assert XA.ndim==2 and XB.ndim==2, "Arrays must be 2-dimensional"
     assert XA.shape[0]==XB.shape[0], "Arrays must have equal number of rows"
     assert XA.shape[1]==XB.shape[0], "Arrays must have equal number of columns"
-
 	if distance == 'haversine':
 		assert XA.shape[1] >= 2, 'Haversine distance requires at least 2 coordinates per point (lat, lng)'
 		assert XB.shape[1] >= 2, 'Haversine distance requires at least 2 coordinates per point (lat, lng)'
-	return _hausdorff(XA, XB, distances_mapping[distance])
+    distance_function = getattr(distances, distance)
+    return _hausdorff(XA, XB, distance_function)
