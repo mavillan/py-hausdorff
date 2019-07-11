5525f95d0eb85ed7d86bd8d448c60889788f831e
Martín Villanueva <martin.villanueva@usm.cl>
2019-07-11 19:01:06 -0400

    Updates setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,9 @@
-import numpy as np
 from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
-
-ext_modules=[
-    Extension("hausdorff",
-              ["hausdorff.pyx"],
-              libraries=["m"],
-              extra_compile_args = ["-O3", "-ffast-math", "-march=native"],
-              extra_link_args=[]
-              ) 
-]
-
-cythonize("hausdorff.pyx")
 
 setup( 
   name = "hausdorff",
-  include_dirs = [np.get_include(),'include'],
-  cmdclass = {"build_ext": build_ext},
-  ext_modules = ext_modules
+  version = '0.2',
+  packages = ['hausdorff',],
+  license='GNU GENERAL PUBLIC LICENSE Version 3',
+  long_description=open('./README.md').read(),
 )
