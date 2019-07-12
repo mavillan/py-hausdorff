18ac9b73d7166f56b8ea2db12c16389fea0acbe2
Martín Villanueva <martin.villanueva@usm.cl>
2019-07-12 00:16:40 -0400

    Adds setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup( 
   name = "hausdorff",
   version = '0.2',
-  packages = ['hausdorff',],
+  author = 'mavillan',
+  author_email = 'nallivam@gmail.com',
+  packages=find_packages(),
   license='GNU GENERAL PUBLIC LICENSE Version 3',
   long_description=open('./README.md').read(),
 )
