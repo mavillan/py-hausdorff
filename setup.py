a67018d46c5a8504ad25a33e66a21637ab3c6bb8
Martín Villanueva <martin.villanueva@usm.cl>
2019-07-12 17:30:49 -0400

    Changes long description of project

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
   author_email = 'nallivam@gmail.com',
   packages=find_packages(),
   license='GNU GENERAL PUBLIC LICENSE Version 3',
-  long_description=open('./README.md').read(),
+  long_description="""
+  This package implements the algorithm presented in An Efficient Algorithm for Calculating the Exact Hausdorff Distance (DOI: [10.1109/TPAMI.2015.2408351](https://doi.org/10.1109/TPAMI.2015.2408351)) by Aziz and Hanbury, 2015.
+  """,
 )
